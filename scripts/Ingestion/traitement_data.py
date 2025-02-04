adde936071ebc06875f482b005cf0ddcd8dd24a2
TriguiB <brahimtrigui52@gmail.com>
2025-02-04 22:04:55 +0100

    docker

--- scripts/Ingestion/traitement_data.py
+++ scripts/Ingestion/traitement_data.py
@@ -1,14 +1,22 @@
 from pyspark.sql import SparkSession
 
-# Ajouter explicitement le connecteur MongoDB pour Spark
+# Création de la session Spark avec MongoDB
 spark = SparkSession.builder \
-    .master("spark://spark-master:7077") \
-    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.1.1") \
-    .config("spark.mongodb.input.uri", "mongodb://mongodb:27017/github_issues.issues") \
-    .config("spark.mongodb.output.uri", "mongodb://mongodb:27017/github_issues.issues") \
+    .appName("MongoDB_Spark") \
+    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.4.1")\
+    .config("spark.mongodb.read.connection.uri", "mongodb://mongodb:27017/github_issues") \
+    .config("spark.mongodb.write.connection.uri", "mongodb://mongodb:27017/github_issues") \
+    .config("spark.executor.memory", "1G") \
+    .config("spark.executor.cores", "1") \
+    .config("spark.cores.max", "4") \
+    .config("spark.driver.memory", "2G") \
+    .config("spark.sql.shuffle.partitions", "4") \
     .getOrCreate()
 
-print(spark.version)  # Vérifier que Spark fonctionne bien
-# Test : Lire les données MongoDB
-df = spark.read.format("mongodb").load()
-df.show(5)  # Affiche les 5 premières lignes
+# Lire les données depuis MongoDB
+df = spark.read.format("mongodb") \
+    .option("database", "github_issues") \
+    .option("collection", "issues") \
+    .load()
+
+df.show()
